4fc860547536934944ee5c5f52364bf1eeaa24a2
kero0 <kbakheat@gmail.com>
2024-04-01 12:42:00 -0400

    Git Sign by default

--- hardware/Kirols-xps9575/home/default.nix
+++ hardware/Kirols-xps9575/home/default.nix
@@ -3,6 +3,8 @@
     ./aliases.nix
   ];
 
+  programs.git.extraConfig.user.signingKey = "9E8CA5ADA77C3B787B4D3A294D004B9A43E3108F";
+
   xresources = {
     properties = {
       "*.dpi" = 192;
--- home-manager/dev-stuff/shells/shell-tools/git.nix
+++ home-manager/dev-stuff/shells/shell-tools/git.nix
@@ -1,19 +1,13 @@
-{ pkgs, ... }: {
+{ pkgs, config, ... }: {
   programs.git = {
     package = pkgs.gitAndTools.gitFull;
     enable = true;
     userName = "kero0";
     userEmail = "kbakheat@gmail.com";
-    includes = [
-      {
-        condition = "gitdir:~/seniordesign";
-        contents = {
-          user = {
-            name = "KirolsAtSchool";
-            email = "bakheakm@udmercy.edu";
-          };
-        };
-      }
+    ignores = [
+      "result"
+      ".DS_STORE"
     ];
+    extraConfig = { commit.gpgSign = true; };
   };
 }
