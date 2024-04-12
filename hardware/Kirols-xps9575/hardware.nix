fa4cbb060c8af10b202547497d9c6fae97afff51
kero0 <kbakheat@gmail.com>
2024-04-12 10:19:56 -0400

    XPS Power Changes

--- hardware/Kirols-xps9575/hardware.nix
+++ hardware/Kirols-xps9575/hardware.nix
@@ -62,7 +62,7 @@
       CPU_SCALING_GOVERNOR_ON_AC = "performance";
       CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
 
-      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "powersave";
       CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
 
       CPU_MIN_PERF_ON_AC = 0;
@@ -74,6 +74,8 @@
       START_CHARGE_THRESH_BAT0 = 40; # 40 and bellow it starts to charge
       STOP_CHARGE_THRESH_BAT0 = 80; # 80 and above it stops charging
 
+      CPU_BOOST_ON_AC = 1;
+      CPU_BOOST_ON_BAT = 0;
     };
   };
 
