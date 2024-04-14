1acc593e0024bdb039dc61cce9d7ba5a1bd4eb96
kero0 <kbakheat@gmail.com>
2024-04-14 12:15:06 -0400

    Cleanup old scripts

--- packages/scripts/default.nix
+++ packages/scripts/default.nix
@@ -7,18 +7,7 @@ with pkgs; let
     installPhase = ''
       #NixOS stuff
       makeWrapper $src/delete-old-generations $out/bin/delete-old-generations
-
-      #Others
-      makeWrapper $src/getwindowname $out/bin/getwindowname --prefix PATH : ${lib.makeBinPath [ libnotify xorg.xprop ]}
-      makeWrapper $src/random-wallpaper $out/bin/random-wallpaper --prefix PATH : ${lib.makeBinPath [ feh ]}
-      makeWrapper $src/rofi-ffmpeg-screenshot $out/bin/rofi-ffmpeg-screenshot --prefix PATH : ${lib.makeBinPath [ bash slop ffcast xclip libnotify ffmpeg-full xorg.xwininfo xdg-user-dirs ]}
-
     '' + lib.strings.optionalString pkgs.stdenv.isLinux "makeWrapper $src/send-remote-audio $out/bin/send-remote-audio --prefix PATH : ${lib.makeBinPath [ ffmpeg pulseaudio ]}";
   };
-
 in
-{
-  environment.systemPackages = [
-    myscripts
-  ];
-}
+  { environment.systemPackages = [ myscripts ]; }
