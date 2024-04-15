38497160ba67bfd3dab6578500f91b3920fe880d
kero0 <kbakheat@gmail.com>
2024-04-15 18:33:30 -0400

    Fix umport exclude

--- umport.nix
+++ umport.nix
@@ -1,59 +1,23 @@
 nixpkgs:
 let
   umport =
-    { path ? null
-    , paths ? [ ]
-    , include ? [ ]
+    { ipath ? null
     , exclude ? [ ]
     , recursive ? true
     ,
     }:
       with nixpkgs.lib;
+      with nixpkgs.lib.path;
       with nixpkgs.lib.fileset; let
-        excludedFiles = filter (path: pathIsRegularFile path) exclude;
-        excludedDirs = filter (path: pathIsDirectory path) exclude;
-        isExcluded = path:
-          if elem path excludedFiles
+        excludedFiles = filter (f: pathIsRegularFile f) exclude;
+        excludedDirs = filter (d: pathIsDirectory d) exclude;
+        isExcluded = f:
+          if elem f excludedFiles
           then true
-          else (filter (excludedDir: path.hasPrefix excludedDir path) excludedDirs) != [ ];
+          else builtins.foldl' (acc: f': acc || hasPrefix f' f) false excludedDirs;
 	in
-	unique (
-          (
-            filter
-              (file: pathIsRegularFile file && hasSuffix ".nix" (builtins.toString file) && !isExcluded file)
-              (concatMap
-		(
-                  _path:
-                if recursive
-                then toList _path
-                else
-                  mapAttrsToList
-                    (
-                      name: type:
-                      _path
-                      + (
-                        if type == "directory"
-                        then "/${name}/default.nix"
-                        else "/${name}"
-                      )
-                    )
-                    (
-                      if excludeDotPaths
-                      then removeDotPaths (builtins.readDir _path)
-                      else builtins.readDir _path
-                    )
-              )
-              (unique (
-                if path == null
-                then paths
-                else [ path ] ++ paths
-              )))
-        )
-        ++ (
-          if recursive
-          then concatMap (path: toList path) (unique include)
-          else unique include
-        )
-      );
+	filter
+          (file: pathIsRegularFile file && hasSuffix ".nix" (builtins.toString file) && !isExcluded file)
+          (filesystem.listFilesRecursive ipath);
   in
   umport
