dabcb0451ab84377a3d9cd8797901d420608d18d
kero0 <kbakheat@gmail.com>
2024-05-24 11:20:07 -0400

    Kanshi remove config for mars and fix warning

--- hardware/mars/home/kanshi.nix
+++ hardware/mars/home/kanshi.nix
@@ -1,39 +1,8 @@
 { pkgs, ... }:
 {
   services.kanshi = {
-    enable = true;
+    enable = false;
     systemdTarget = "graphical-session.target";
-    profiles = {
-      default = {
-        outputs = [
-          {
-            criteria = "eDP-1";
-            mode = "1920x1080";
-            position = "0,0";
-            scale = 1.2;
-          }
-        ];
-      };
-      docked = {
-        outputs = [
-          {
-            criteria = "*";
-            mode = "2560x1440";
-            position = "0,20";
-          }
-          {
-            criteria = "*";
-            mode = "1920x1200";
-            position = "2560,20";
-          }
-          {
-            criteria = "eDP-1";
-            mode = "1920x1080";
-            position = "4480,0";
-            scale = 1.2;
-          }
-        ];
-      };
-    };
+    # settings = { };
   };
 }
