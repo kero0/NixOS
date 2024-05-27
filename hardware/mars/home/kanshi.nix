34b9be60544251ee5dfb806a5d03fb3e47069fde
kero0 <kbakheat@gmail.com>
2024-05-27 11:51:30 -0400

    readd kanshi config

--- hardware/mars/home/kanshi.nix
+++ hardware/mars/home/kanshi.nix
@@ -1,8 +1,45 @@
 { pkgs, ... }:
 {
   services.kanshi = {
-    enable = false;
+    enable = true;
     systemdTarget = "graphical-session.target";
-    # settings = { };
+    settings = [
+      {
+        profile = {
+          name = "default";
+          outputs = [
+            {
+              criteria = "eDP-1";
+              mode = "1920x1080";
+              position = "0,0";
+              scale = 1.0;
+            }
+          ];
+        };
+      }
+      {
+        profile = {
+          name = "docked";
+          outputs = [
+            {
+              criteria = "*";
+              mode = "2560x1440";
+              position = "0,20";
+            }
+            {
+              criteria = "*";
+              mode = "1920x1200";
+              position = "2560,20";
+            }
+            {
+              criteria = "eDP-1";
+              mode = "1920x1080";
+              position = "4480,0";
+              scale = 1.0;
+            }
+          ];
+        };
+      }
+    ];
   };
 }
