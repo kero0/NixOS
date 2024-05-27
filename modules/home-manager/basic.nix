aa4c1048857dd076c931c5ffeac25e2b0122fc4f
kero0 <kbakheat@gmail.com>
2024-05-27 19:32:34 -0400

    Fix darwin

--- modules/home-manager/basic.nix
+++ modules/home-manager/basic.nix
@@ -1,9 +1,4 @@
-{
-  pkgs,
-  config,
-  osConfig,
-  ...
-}:
+{ pkgs, osConfig, ... }:
 {
   home.stateVersion = "22.05";
 
@@ -18,21 +13,5 @@
     enable = true;
     path = osConfig.my.user.homedir + /.config;
   };
-
-  xdg = {
-    enable = true;
-    userDirs = {
-      enable = true;
-      createDirectories = true;
-
-      desktop = "${config.home.homeDirectory}/Desktop";
-      documents = "${config.home.homeDirectory}/Documents";
-      download = "${config.home.homeDirectory}/Downloads";
-      music = "${config.home.homeDirectory}/Music";
-      pictures = "${config.home.homeDirectory}/Pictures";
-      publicShare = "${config.home.homeDirectory}/Public";
-      templates = "${config.home.homeDirectory}/Templates";
-      videos = "${config.home.homeDirectory}/Videos";
-    };
-  };
+  xdg.enable = true;
 }
---
+++ modules/home-manager/nixos-specific/basic.nix
@@ -0,0 +1,18 @@
+{ config, ... }:
+{
+  xdg = {
+    userDirs = {
+      enable = true;
+      createDirectories = true;
+
+      desktop = "${config.home.homeDirectory}/Desktop";
+      documents = "${config.home.homeDirectory}/Documents";
+      download = "${config.home.homeDirectory}/Downloads";
+      music = "${config.home.homeDirectory}/Music";
+      pictures = "${config.home.homeDirectory}/Pictures";
+      publicShare = "${config.home.homeDirectory}/Public";
+      templates = "${config.home.homeDirectory}/Templates";
+      videos = "${config.home.homeDirectory}/Videos";
+    };
+  };
+}
