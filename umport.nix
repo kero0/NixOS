d94226ae725cc35b98e2eb5da0150b8cd17fd80c
kero0 <kbakheat@gmail.com>
2024-06-28 21:44:02 -0400

    simplify umport

--- umport.nix
+++ umport.nix
@@ -1,26 +1,16 @@
 nixpkgs:
+{
+  ipath ? null,
+  exclude ? [ ],
+}:
+with nixpkgs.lib;
+with nixpkgs.lib.path;
+with nixpkgs.lib.fileset;
 let
-  umport =
-    {
-      ipath ? null,
-      exclude ? [ ],
-      recursive ? true,
-    }:
-    with nixpkgs.lib;
-    with nixpkgs.lib.path;
-    with nixpkgs.lib.fileset;
-    let
-      excludedFiles = filter (f: pathIsRegularFile f) exclude;
-      excludedDirs = filter (d: pathIsDirectory d) exclude;
-      isExcluded =
-        f:
-        if elem f excludedFiles then
-          true
-        else
-          builtins.foldl' (acc: f': acc || hasPrefix f' f) false excludedDirs;
-    in
-    filter (
-      file: pathIsRegularFile file && hasSuffix ".nix" (builtins.toString file) && !isExcluded file
-    ) (filesystem.listFilesRecursive ipath);
+  excludedFiles = filter pathIsRegularFile exclude;
+  excludedDirs = filter pathIsDirectory exclude;
+  isExcluded = f: elem f excludedFiles || any (flip hasPrefix f) excludedDirs;
 in
-umport
+filter (
+  file: pathIsRegularFile file && hasSuffix ".nix" (builtins.toString file) && !isExcluded file
+) (filesystem.listFilesRecursive ipath)
