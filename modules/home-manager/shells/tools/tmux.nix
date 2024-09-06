5cbcfba0b97ef40bbe23b0fc1613dc00952a99cc
kero0 <kbakheat@gmail.com>
2024-09-05 22:22:13 -0400

    tmux add new keybinds

--- modules/home-manager/shells/tools/tmux.nix
+++ modules/home-manager/shells/tools/tmux.nix
@@ -47,6 +47,16 @@ in
         extraConfig = ''
           bind-key x kill-pane # skip "kill-pane 1? (y/n)" prompt
           set -g detach-on-destroy off  # don't exit from tmux when closing a session
+          bind | split-window -h -c "#{pane_current_path}"
+          bind - split-window -v -c "#{pane_current_path}"
+
+          is_vim="ps -o state= -o comm= -t '#{pane_tty}' \
+            | grep -iqE '^[^TXZ ]+ +(\\S+\\/)?g?(view|n?vim?x?)(diff)?$'"
+          bind-key -n C-h  if-shell  "$is_vim"  "send-keys C-h"  "select-pane -L"
+          bind-key -n C-j   if-shell  "$is_vim"  "send-keys C-j"   "select-pane -D"
+          bind-key -n C-k  if-shell  "$is_vim"  "send-keys C-k"  "select-pane -U"
+          bind-key -n C-l   if-shell  "$is_vim"  "send-keys C-l"   "select-pane -R"
+          bind-key -n C-\   if-shell  "$is_vim"  "send-keys C-\\"  "select-pane -l"
         '';
         catppuccin = {
           enable = true;
