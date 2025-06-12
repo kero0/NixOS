262f1d5be4adfa1317d6d1996d2634b527bf96cc
kero0 <kbakheat@gmail.com>
2025-06-12 19:26:24 -0400

    update macbook signing key

--- hardware/Kirolss-MacBook-Pro/home/main.nix
+++ hardware/Kirolss-MacBook-Pro/home/main.nix
@@ -1,6 +1,6 @@
 {
   programs.git.extraConfig = {
     github.user = "kero0";
-    user.signingKey = "57EFE5CBEBF23B57751B8C48250CB991DD15CDE0";
+    user.signingKey = "8CC80E4BB584DBB46EA9F284514638168B6842B2";
   };
 }
