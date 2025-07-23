53fc6cb34f59caa59fcf9a9dc2e699e566ba4667
kero0 <kbakheat@gmail.com>
2025-07-22 22:09:17 -0400

    remove pipe usage

--- modules/home-manager/nixos-specific/browser.nix
+++ modules/home-manager/nixos-specific/browser.nix
@@ -11,34 +11,35 @@ in
 {
   options.my.home.browser.enable = mkEnableOption "Enable browser config";
   config = mkIf cfg.enable {
-    programs =
-      [
-        "chromium"
-        "brave"
-        "vivaldi"
-      ]
-      |> map (pkg: {
-        ${pkg} = {
-          package = pkgs.${pkg}.override {
-            commandLineArgs =
-              if pkgs.stdenv.isLinux then
-                "--enable-features=TouchpadOverscrollHistoryNavigation,VaapiVideoDecode --ignore-gpu-blocklist --enable-gpu-rasterization"
-              else
-                null;
+    programs = attrsets.mergeAttrsList (
+      map
+        (pkg: {
+          ${pkg} = {
+            package = pkgs.${pkg}.override {
+              commandLineArgs =
+                if pkgs.stdenv.isLinux then
+                  "--enable-features=TouchpadOverscrollHistoryNavigation,VaapiVideoDecode --ignore-gpu-blocklist --enable-gpu-rasterization"
+                else
+                  null;
+            };
+            enable = true;
+            extensions = [
+              # ublock origin
+              { id = "cjpalhdlnbpafiamejdnhcphjbkeiagm"; }
+              # bitwarden
+              { id = "nngceckbapebfimnlniiiahkandclblb"; }
+              # video speed controller
+              { id = "nffaoalbilbmmfgbnbgppjihopabppdk"; }
+              # sponsorblock
+              { id = "mnjggcdmjocbbbhaepdhchncahnbgone"; }
+            ];
           };
-          enable = true;
-          extensions = [
-            # ublock origin
-            { id = "cjpalhdlnbpafiamejdnhcphjbkeiagm"; }
-            # bitwarden
-            { id = "nngceckbapebfimnlniiiahkandclblb"; }
-            # video speed controller
-            { id = "nffaoalbilbmmfgbnbgppjihopabppdk"; }
-            # sponsorblock
-            { id = "mnjggcdmjocbbbhaepdhchncahnbgone"; }
-          ];
-        };
-      })
-      |> attrsets.mergeAttrsList;
+        })
+        [
+          "brave"
+          "chromium"
+          "vivaldi"
+        ]
+    );
   };
 }
