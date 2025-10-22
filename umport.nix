349e7a12a6fac2c8545c9b1061a8d67e9f4be427
kero0 <kbakheat@gmail.com>
2025-10-21 23:13:12 -0400

    fix bad function call in umport

--- umport.nix
+++ umport.nix
@@ -7,8 +7,8 @@ with nixpkgs.lib;
 with nixpkgs.lib.path;
 with nixpkgs.lib.fileset;
 let
-  excludedFiles = filter (f: isPath f && pathIsRegularFile) exclude;
-  excludedDirs = filter (f: isPath f && pathIsDirectory) exclude;
+  excludedFiles = filter (f: isPath f && pathIsRegularFile f) exclude;
+  excludedDirs = filter (f: isPath f && pathIsDirectory f) exclude;
   excludedRegexes = path: any (r: null != match r (toString path)) (filter (f: !isPath f) exclude);
   isExcluded = f: elem f excludedFiles || any (flip hasPrefix f) excludedDirs || excludedRegexes f;
 in
