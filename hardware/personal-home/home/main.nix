8b6a3cbcacc05d5fa255a927edd08b761608c9c4
kero0 <kbakheat@gmail.com>
2025-11-04 23:34:10 -0500

    update fixes

--- hardware/personal-home/home/main.nix
+++ hardware/personal-home/home/main.nix
@@ -2,7 +2,7 @@
 {
   programs.git = {
     lfs.enable = true;
-    extraConfig = {
+    settings = {
       github.user = "kero0";
       user.signingKey = "6203EA1E2A444A37709BF65023B20E88C6992499";
       push.autoSetupRemote = true;
