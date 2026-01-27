ec0f384a7990cfa0c9b70920896f68362a226f8d
kero0 <kbakheat@gmail.com>
2026-01-26 23:14:01 -0500

    add new latex packages

--- modules/home-manager/school.nix
+++ modules/home-manager/school.nix
@@ -30,35 +30,83 @@ in
         };
       };
     };
-    home.packages = [
-      (
-        with pkgs.texlive;
-        combine {
-          inherit
-            scheme-small
-            biblatex
-            dvisvgm
-            latexmk
-            ;
-          inherit
-            babel
-            capt-of
-            environ
-            everypage
-            float
-            fvextra
-            needspace
-            pdfcol
-            siunitx
-            soul
-            standalone
-            subfiles
-            tcolorbox
-            wrapfig
-            xcolor
-            ;
-        }
-      )
-    ];
+    home.packages =
+      let
+        jdf = pkgs.stdenvNoCC.mkDerivation {
+          name = "jdf";
+          src = pkgs.fetchFromGitHub {
+            owner = "iamjakewarner";
+            repo = "jdf";
+            rev = "4456167bfec748fcf4bede166df7b9c5a2e56a6b";
+            hash = "sha256-uJKgo8ZQDGlc+hgVVkmVvW+XPyf/St8lxPjQBaY9egI=";
+          };
+          nativeBuildInputs = [
+            (pkgs.writeShellScript "force-tex-output.sh" ''
+              out="''${tex-}"
+            '')
+          ];
+          outputs = [ "tex" ];
+          dontConfigure = true;
+
+          installPhase = ''
+            path="$tex/tex/generic/jdf"
+            mkdir -p "$path"
+            cp $src/jdf.cls "$path"/jdf.cls
+          '';
+        };
+      in
+      [
+        (
+          with pkgs.texlive;
+          combine {
+            inherit
+              scheme-small
+              biblatex
+              dvipng
+              dvisvgm
+              latexmk
+              ;
+            inherit
+              amsmath
+              babel
+              capt-of
+              caption
+              changepage
+              cm-super
+              csquotes
+              enumitem
+              environ
+              etoolbox
+              eulervm
+              everypage
+              float
+              footmisc
+              footnotebackref
+              fvextra
+              geometry
+              hyperref
+              jdf
+              ly1
+              mathpazo
+              mdframed
+              microtype
+              needspace
+              nowidow
+              pdfcol
+              siunitx
+              soul
+              sourcecodepro
+              standalone
+              subfiles
+              tcolorbox
+              titlesec
+              type1cm
+              wrapfig
+              xcolor
+              zref
+              ;
+          }
+        )
+      ];
   };
 }
