2c0b887cf96dd87589be5c09833636a37b69bb56
kero0 <kbakheat@gmail.com>
2026-02-04 08:21:18 -0500

    cleanp unused hombrew casks

--- hardware/Kirolss-MacBook-Pro/nixos/homebrew.nix
+++ hardware/Kirolss-MacBook-Pro/nixos/homebrew.nix
@@ -13,19 +13,14 @@
     };
     greedyCasks = true;
     casks = [
-      "appcleaner"
       "bambu-studio"
-      "disk-inventory-x"
       "hammerspoon"
       "maccy"
-      "nordvpn"
       "orion"
       "the-unarchiver"
       "unnaturalscrollwheels"
       "vivaldi"
       "vlc"
-      "wine@staging"
-      "zen"
     ];
   };
   environment.systemPath = [ config.homebrew.brewPrefix ];
