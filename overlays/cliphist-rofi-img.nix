e9e68c23ac84406d80b7666389fe417ce7a60c5a
kero0 <44583148+kero0@users.noreply.github.com>
2026-03-20 00:59:12 +0000

    automated package update

--- overlays/cliphist-rofi-img.nix
+++ overlays/cliphist-rofi-img.nix
@@ -1,13 +1,13 @@
 final: prev: with prev.lib; {
   cliphist-rofi-img = final.pkgs.stdenv.mkDerivation rec {
     pname = "cliphist-rofi-img";
-    version = "unstable-2026-01-26";
+    version = "unstable-2026-03-09";
 
     src = final.pkgs.fetchFromGitHub {
       owner = "sentriz";
       repo = "cliphist";
-      rev = "bae528ea9fbd9a2b51a71ff2ea0e5ac323ebc542";
-      sha256 = "1nlfq5zq8cmbnzcflrlqdz28vjnkx5dkp3637y4lwygv0qv9ijdm";
+      rev = "a17fb70fdc5a46917c6125ff49c7624491d6bf82";
+      sha256 = "025bf98p64gyc9vvayh8y2rv0fwmbx64bixm3aafa48apj7ijr6n";
     };
 
     buildInputs = with final.pkgs; [
