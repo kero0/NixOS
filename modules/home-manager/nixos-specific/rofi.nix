0a3333e0d120e88f916b5b8c231bc91975adf770
kero0 <kbakheat@gmail.com>
2026-03-27 21:37:54 -0400

    rofi improvements

--- modules/home-manager/nixos-specific/rofi.nix
+++ modules/home-manager/nixos-specific/rofi.nix
@@ -13,21 +13,27 @@ in
   config = mkIf cfg.enable {
     programs.rofi = pkgs.lib.mkIf pkgs.stdenv.isLinux {
       enable = true;
-      font = "JetBrainsMono Nerd Font 14";
-      terminal = "${pkgs.kitty}/bin/kitty";
+      font = "JetBrainsMono Nerd Font 28";
+      terminal = lib.mkIf config.programs.kitty.enable "kitty";
       cycle = true;
       location = "center";
+      modes = [
+        "drun"
+        "run"
+      ]
+      ++ lists.optional config.my.home.clipboard-manager.enable {
+        name = "clipboard";
+        path = "${pkgs.cliphist}/bin/cliphist-rofi-img";
+      };
       theme = {
         "#window" = {
           fullscreen = true;
         };
       };
       extraConfig = {
-        modi = "drun,run,window,ssh,";
         kb-primary-paste = "Control+V,Shift+Insert";
         kb-secondary-paste = "Control+v,Insert";
         matching = "regex";
-        dpi = 1;
       };
     };
   };
