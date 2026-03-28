685cee5f781928448198ab162394eb83b6a786e7
kero0 <kbakheat@gmail.com>
2026-03-27 20:27:20 -0400

    fix float rules

--- modules/home-manager/nixos-specific/hyprland/windowrules.nix
+++ modules/home-manager/nixos-specific/hyprland/windowrules.nix
@@ -7,11 +7,20 @@ with lib;
 {
   config = mkIf config.my.home.hyprland.enable {
     wayland.windowManager.hyprland.settings = {
-      windowrule = map (x: "float, pin, size 35% 35%, move 30% 20%, ${x}") [
-        "class:Bitwarden"
-        "class:.*-nngceckbapebfimnlniiiahkandclblb-.*" # bitwarden browser ext
-        "class:xdg-desktop-portal-gtk"
-      ];
+      windowrule =
+        map
+          (x: {
+            name = "float-${x}";
+            "match:class" = x;
+            float = "on";
+            size = "(monitor_w*0.7) (monitor_h*0.7)";
+            move = "(monitor_w*0.15) (monitor_h*0.1)";
+          })
+          [
+            "Bitwarden"
+            ".*-nngceckbapebfimnlniiiahkandclblb-.*" # bitwarden browser ext
+            "xdg-desktop-portal-gtk"
+          ];
     };
   };
 }
