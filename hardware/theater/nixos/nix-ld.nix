adffc0b8845bc65fe9ae81e4776c0d768da42c02
kero0 <kbakheat@gmail.com>
2026-09-02 18:04:48 -0400

    comment out gtk2 components in nix-ld

--- hardware/theater/nixos/nix-ld.nix
+++ hardware/theater/nixos/nix-ld.nix
@@ -120,8 +120,8 @@
       SDL2_ttf
       SDL2_mixer
       # libappindicator-gtk2
-      libdbusmenu-gtk2
-      libindicator-gtk2
+      # libdbusmenu-gtk2
+      # libindicator-gtk2
       libcaca
       libcanberra
       libgcrypt
